91a745d805972b19b6380f2b0aa6a2dc578c5789
HarisR39 <fireplatypus375@gmail.com>
2023-01-04 23:13:52 -0500

    search option

--- fbla project/test2.py
+++ fbla project/test2.py
@@ -1,18 +1,13 @@
 from tkinter import *
 import tkinter as tk
 import tkinter.ttk as ttk
-import customtkinter 
+import customtkinter as ctk
 
-app = customtkinter.CTk()
-optionmenu_var = customtkinter.StringVar(value="option 2")  # set initial value
-
-def optionmenu_callback(choice):
-    print("optionmenu dropdown clicked:", choice)
-
-combobox = customtkinter.CTkComboBox(master=app,
-                                     values=["1", "2"],
-                                     variable=optionmenu_var)
-combobox.pack(padx=20, pady=10)
+window = tk.Tk()
+my_entry = ctk.CTkEntry(window)
+my_entry.insert(tk.END, "default text")
+my_entry.pack()
+my_entry.bind("<Button-1>", lambda a: my_entry.delete(0, tk.END))
 if __name__ == "__main__":
-    app.mainloop()
+    window.mainloop()
 

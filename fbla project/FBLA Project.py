59271a5fea58bf59ed816cd1406ba93752cb1d48
HarisR39 <fireplatypus375@gmail.com>
2023-01-12 20:37:06 -0500

    rand student works

--- fbla project/FBLA Project.py
+++ fbla project/FBLA Project.py
@@ -466,29 +466,16 @@ def pickWinner():
 
     for grade in range(9, 13):
         cursor.execute("SELECT * FROM students WHERE grade = :grade", {'grade': grade})
-        gradeValues= cursor.fetchall()
-        gradeMax = list()
-        max2 = 0
-        for i in range(len(gradeValues)):
-            if gradeValues[i][3] > max2:
-                max2 = gradeValues[i][3]
-        for i in range(len(gradeValues)):
-            if max2 == gradeValues[i][3]:
-                gradeMax.append(gradeValues[i])
-        print(gradeMax)
+        gradeStudents = cursor.fetchall()
+        randStudent = rand.randint(0, len(gradeStudents)-1)
+        space = ctk.CTkLabel(win, text= " ")
+        space.pack()
+        label5= ctk.CTkLabel(win, text= "The random winner for grade " + str(grade) + " is: ")
+        label5.pack()
+        label6 = ctk.CTkLabel(win, text= f" {(gradeStudents[randStudent][0]).title()} {(gradeStudents[randStudent][1][0]).title()}. with a total of: {gradeStudents[randStudent][3]} points.")
+        label6.pack()
 
-        if len(gradeMax) > 1:
-            Label5 = ctk.CTkLabel(win, text= "The students that tied for first place in grade " + str(grade) + " are:" , corner_radius=10)
-            Label5.pack()
-            for i in range(len(gradeMax)):
-                Labe6 = ctk.CTkLabel(win, text=f" {(gradeMax[i][0]).title()} {(gradeMax[i][1][0]).title()}. with a total of: {gradeMax[i][3]} points.", corner_radius=10)
-                Labe6.pack()
-        else:
-            Label7 = ctk.CTkLabel(win, text= "The student that won first place in grade " + str(grade) + " is: ", corner_radius=10)
-            Label7.pack()
-
-            Label8 = ctk.CTkLabel(win, text=f" {(gradeMax[0][0]).title()} {(gradeMax[0][1][0]).title()}. with a total of: {gradeMax[0][3]} points.", corner_radius=10)
-            Label8.pack()
+        
 
 
 
--- fbla project/asd kjhasnlksdfaskjlfds.py
+++ fbla project/asd kjhasnlksdfaskjlfds.py
@@ -1,25 +1,25 @@
-import matplotlib.pyplot as plt
+for grade in range(9, 13):
+    cursor.execute("SELECT * FROM students WHERE grade = :grade", {'grade': grade})
+    gradeValues= cursor.fetchall()
+    gradeMax = list()
+    max2 = 0
+    for i in range(len(gradeValues)):
+        if gradeValues[i][3] > max2:
+            max2 = gradeValues[i][3]
+    for i in range(len(gradeValues)):
+        if max2 == gradeValues[i][3]:
+            gradeMax.append(gradeValues[i])
+    print(gradeMax)
 
-# Define the data
-data=[10,0,0,0,0,0,0,0,0,0,0,0,0,0]
-names = ["Aidan","Bryce","Cow","Dog","Easter","Fstring","Gargantuan","H name name","Illidan","Jungler","absds","dfgsdfg","dfgd34t","g vbfd"]
+    if len(gradeMax) > 1:
+        Label5 = ctk.CTkLabel(win, text= "The students that tied for first place in grade " + str(grade) + " are:" , corner_radius=10)
+        Label5.pack()
+        for i in range(len(gradeMax)):
+            Labe6 = ctk.CTkLabel(win, text=f" {(gradeMax[i][0]).title()} {(gradeMax[i][1][0]).title()}. with a total of: {gradeMax[i][3]} points.", corner_radius=10)
+            Labe6.pack()
+    else:
+        Label7 = ctk.CTkLabel(win, text= "The student that won first place in grade " + str(grade) + " is: ", corner_radius=10)
+        Label7.pack()
 
-# Sort the data in decreasing order
-#data = sorted(data, reverse=True)
-
-# Create the chart\
-
-plt.rcParams["figure.figsize"] = (15,5)
-plt.barh(names,data,color="royalblue")
-
-
-
-#plt.plot(range(len(data)), data,"r+")
-plt.savefig("QPoints.pdf",format="pdf")
-
-plt.title('Quarterly Points (10)', fontweight="bold")
-
-plt.ylabel('Students', fontweight="bold")
-plt.xlabel('Points', fontweight="bold")
-
-plt.show()
+        Label8 = ctk.CTkLabel(win, text=f" {(gradeMax[0][0]).title()} {(gradeMax[0][1][0]).title()}. with a total of: {gradeMax[0][3]} points.", corner_radius=10)
+        Label8.pack()

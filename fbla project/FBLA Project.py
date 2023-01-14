33ab09ba8e0ae265c42e0afe97ea9d18c6895116
Haris <73035095+HarisR39@users.noreply.github.com>
2023-01-13 22:03:47 -0500

    Add files via upload

--- fbla project/FBLA Project.py
+++ fbla project/FBLA Project.py
@@ -456,7 +456,8 @@ def report():
 
 #command for pick winners button
 def pickWinner():
-    gradeValues = list()
+    prizes = ["free spirit gear", "a day of excused school", "free admission to all school games for a quarter", "free lunch for a week"]
+    studentValues = list()
     cursor.execute("SELECT * FROM students")
     all_students = cursor.fetchall()
     pointList = list()
@@ -473,7 +474,7 @@ def pickWinner():
     # GUI
     win = ctk.CTkToplevel()
     win.title("Winners")
-    win.geometry("400x400")
+    win.geometry("400x800")
 
     #prints the winner if there is multiple winners
     if len(maxStudents) > 1:
@@ -497,12 +498,44 @@ def pickWinner():
             cursor.execute("SELECT * FROM students WHERE grade = :grade", {'grade': grade})
             gradeStudents = cursor.fetchall()
             randStudent = rand.randint(0, len(gradeStudents)-1)
+            studentValues.append(gradeStudents[randStudent])
             space = ctk.CTkLabel(win, text= " ")
             space.pack()
             label5= ctk.CTkLabel(win, text= "The random winner for grade " + str(grade) + " is: ")
             label5.pack()
             label6 = ctk.CTkLabel(win, text= f" {(gradeStudents[randStudent][0]).title()} {(gradeStudents[randStudent][1][0]).title()}. with a total of: {gradeStudents[randStudent][3]} points.")
             label6.pack()  
+
+        space1 = ctk.CTkLabel(win, text= "")
+        space1.pack()
+        space2 = ctk.CTkLabel(win, text= "")
+        space2.pack()
+        rand.shuffle(studentValues)
+        print(studentValues)
+
+        if len(maxStudents) > 1:
+            label7 = ctk.CTkLabel(win, text= " These students won a Pizza Party for their class: ")
+            label7.pack()
+            for i in range(len(maxStudents)):
+                Label10 = ctk.CTkLabel(win, text=f" {(maxStudents[i][0]).title()} {(maxStudents[i][1][0]).title()}. ", corner_radius=10)
+                Label10.pack()
+
+    #prints the winner if there is only one winner
+        else:
+            Label11 = ctk.CTkLabel(win, text= "This student won a Pizza Party for their class: ", corner_radius=10)
+            Label11.pack()
+
+            Label12 = ctk.CTkLabel(win, text=f" {(maxStudents[0][0]).title()} {(maxStudents[0][1][0]).title()}.", corner_radius=10)
+            Label12.pack()
+
+        for i in range(len(studentValues)):
+            label14 = ctk.CTkLabel(win, text="")
+            label14.pack()
+            label13 = ctk.CTkLabel(win, text=str(studentValues[i][0]) + " " + str(studentValues[i][1][0]) + ". wins " + prizes[i])
+            label13.pack()
+        
+
+
     except ValueError:
         ...
 
